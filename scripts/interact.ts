c72aefd89036e69db69c112e65eba6f1f69dc890
Esther Breath <estherojile248@gmail.com>
2023-09-10 20:02:49 +0000

    interact

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -1,12 +1,16 @@
 import { ethers } from "hardhat";
 
-const amount2 = ethers.parseEther('0.01')
+const amount = ethers.parseEther('0.01')
+const amount2 = ethers.parseEther('0.02')
+
 
 
 async function main() {
 
     const swap = await ethers.getContractAt('ISwap', '0x95d1FA946BD7655bAcD20F484a0E90C01769E582' )
 
-    await swap.swapToB()
+    await swap.swapToB(amount)
+
+    await swap.swapToA(amount2)
 
   }
